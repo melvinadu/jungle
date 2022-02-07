07b5f7febeb6a562dc165371417b72436c19694d
Melvin Adu <contactmelvinadu@gmail.com>
2022-02-07 21:57:06 +0000

    add User model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 4}
+  validates :password_confirmation, presence: true
+
+  
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+
+    it 'should save when email, name, password fields are filled' do
+
+      @user = User.new(
+        email: 'a@a.com',
+        name: 'John Smith',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+
+      @user.save!
+
+      expect(@user.id).to be_present
+    end
+
+    it 'should save when email, name, password fields are filled and the passwords should be the same' do
+
+      @user = User.new(
+        email: 'aa@a.com',
+        name: 'John Smith',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+
+      @user.save!
+
+      expect(@user.id).to be_present
+      expect(@user.password).to be == (@user.password_confirmation)
+    end
+
+    it 'should not save when email, name, password fields are filled and the passwords are not the same' do
+
+      @user = User.new(
+        email: 'aa@a.com',
+        name: 'John Smith',
+        password: '1234',
+        password_confirmation: '12345'
+      )
+
+      @user.save
+
+      expect(@user.id).not_to be_present
+    end
+
+    it 'should not save when email, name, password fields are filled but the email has already been used (not case-sensitive)' do
+
+      @user = User.new(
+        email: 'aa@a.com',
+        name: 'John Smith',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+
+      @user1 = User.new(
+        email: 'AA@a.com',
+        name: 'John Smith',
+        password: '1234',
+        password_confirmation: '1234'
+      )
+
+      @user.save
+      @user1.save
+
+      expect(@user.id).to be_present
+      expect(@user1.id).not_to be_present
+    end
+    
+  end
+end
